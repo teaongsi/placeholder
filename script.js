10882f427150873479abec40933d4826065a79c7
teaongsi <154658746+teaongsi@users.noreply.github.com>
2024-12-23 13:41:26 +0545

    Initial commit

---
+++ script.js
@@ -0,0 +1,12 @@
+function playAudio() {
+    const volumeIcon = document.getElementById('icon');
+
+    volumeIcon.src = 'images/volume_down.svg';
+
+    const audio = document.getElementById('audio');
+    audio.play();
+
+    setTimeout(() => {
+        volumeIcon.src = 'images/volume_up.svg';
+    }, 750);
+}
